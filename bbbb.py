311d941f6515ab758f0b9ba3fb5511953e0da83c
LeeSongHeon <dlthdgjs987@gmail.com>
2021-03-02 16:38:20 +0900

    first commit

---
+++ bbbb.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import datetime
+
+req = requests.get('https://www.op.gg/champion/statistics')
+raw = req.text
+html = BeautifulSoup(raw, 'html.parser')
+
+champion_name = soup.select('tbody.tabltem.champion-trend-tier-TOP')
+
+
+name = []
+rank = []
+value = []
+for name in champion_name:
+    print(champion_name)
