32b6bc6b04ef0f3677f1538e0126622c7b0d0615
LeeSongHeon <dlthdgjs987@gmail.com>
2021-03-07 21:38:08 +0900

    second opgg commit

--- gfder.py
+++ gfder.py
@@ -7,9 +7,9 @@ import datetime
 # Workbook 생성
 wb = Workbook()
 # sheet 활성
-ws = wb.create_sheet('opgg')
+ws = wb.create_sheet('챔피언 순위')
 # 데이터프레임 내 변수명 생성
-ws.append(['순위', '챔피언', '승률', '픽률'])
+ws.append(['순위', '변동순위', '', '챔피언이름', '승률', '픽률', '티어'])
 # 데이터 크롤링 과정
 response = requests.get("https://www.op.gg/champion/statistics")
 rating_page = response.text
@@ -22,6 +22,9 @@ for tr_tag in soup.select('tr')[1:]:
         td_tags[1].get_text(), #챔피언
         td_tags[2].get_text(), # 승률
         td_tags[3].get_text(), # 픽률
+        td_tags[4].get_text(),
+        td_tags[5].get_text(),
+        td_tags[6].get_text(),
     ]
     # sheet 내 각 행에 데이터 추가
     ws.append(row)
